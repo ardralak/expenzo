cd041cea61ab21c20ee3484069746cbe7892ed8d
Morabad Shreya (MS/EMT4-VM) <Shreya.Morabad@in.bosch.com>
2025-06-18 16:22:48 +0530

    changes in the login jsx and login css

--- src/modules/login.jsx
+++ src/modules/login.jsx
@@ -1,6 +1,8 @@
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import { FileText } from "lucide-react";
-import {Fuel, Utensils, Clapperboard, BriefcaseMedical, CreditCard, PiggyBank, IndianRupee, ShoppingCart, Coins, BarChart3, ChartNoAxesCombined } from 'lucide-react';
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
+import {CreditCard, PiggyBank, IndianRupee, ShoppingCart, Coins, BarChart3, ChartNoAxesCombined } from 'lucide-react';
 import "../styles/login.css";
 
 
@@ -20,9 +22,8 @@ export default function Login() {
       <div className="w-[65%] h-screen bg-gradient-to-tl from-[#103b2f] via-[#0f0f0f] to-[#103b2f] flex flex-col justify-center items-center gap-6">
        
         <h2 className="text-2xl font-semibold mb-4 text-white">Your Expenses</h2>
-        <PiggyBank className="bg-icon piggy" />
-        <BriefcaseMedical className="bg-icon briefcase-medical" />
         
+
         <div className="w-full h-60">
           <ResponsiveContainer width="100%" height="100%">
             <BarChart data={data}>
@@ -36,9 +37,7 @@ export default function Login() {
             </BarChart>
           </ResponsiveContainer>
         </div>
-        <Fuel className="bg-icon fuel" />
-          <Clapperboard className="bg-icon clapperboard" />
-          <Utensils className="bg-icon utensils" />
+        
          <div className="flex gap-4">
           {/* Monthly Report Generation */}
           <div className="w-36 bg-white rounded-xl shadow-md p-4 flex flex-col items-center relative group cursor-pointer">
@@ -59,75 +58,82 @@ export default function Login() {
         </div>
       </div>
 
-  <div className="w-[35%] bg-gradient-to-tl from-[#0f0f0f] via-[#103b2f] to-[#0f0f0f]">
-    <Coins className="bg-icon coin" />
-      <BarChart3 className="bg-icon graph" />
-     
-      <PiggyBank className="bg-icon piggy" />
-      <ShoppingCart className="bg-icon shopping" />
-      <IndianRupee className="bg-icon rupee" />
-      
-       <h2 className="text-xl font-semibold text-center mb-6 text-white">
+  <div className="w-[35%] flex bg-[#1B1D1B] flex-col justify-center p-8">
+        <Coins className="bg-icon coin" />
+        <PiggyBank className="bg-icon piggy" />
+        <ShoppingCart className="bg-icon shopping" />
+        <IndianRupee className="bg-icon rupee" />
+ 
+        <h2 className="text-xl font-semibold text-center mb-8 text-white">
           Sign in to your account
         </h2>
-
-        {/* Form */}
+ 
         <div className="flex justify-center">
           <form className="space-y-5 w-full max-w-xs">
-            {/* Username */}
             <input
               type="text"
               placeholder="Username"
               className="w-full px-4 py-2 bg-[#2a2a2e] text-white border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
             />
-
-            {/* Password */}
             <input
               type="password"
               placeholder="Enter your password"
               className="w-full px-4 py-2 bg-[#2a2a2e] text-white border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
             />
-
-            {/* Remember Me */}
-            <div className="flex w-[100%] space-x-24"> 
-            <div className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                id="remember"
-                className="accent-green-500 w-4 h-4"
-              />
-              <label htmlFor="remember" className="text-xs cursor-pointer text-white">
-                Remember me
-              </label>
-            </div>
-
-            {/* Forgot Password */}
-            <div className="flex justify-end ">
-              <a href="#" className="text-green-400 hover:underline text-xs">
+ 
+            <div className="flex w-full justify-between">
+              <div className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  id="remember"
+                  className="accent-green-500 w-4 h-4"
+                />
+                <label htmlFor="remember" className="text-sm cursor-pointer text-white">
+                  Remember me
+                </label>
+              </div>
+              <a href="#" className="text-green-400 text-sm hover:underline">
                 Forgot password?
               </a>
             </div>
-            </div>
-
-            {/* Login Button */}
+ 
             <button
               type="submit"
               className="w-full bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded"
             >
               Login
             </button>
-
-            {/* Sign Up Link */}
+ 
+            <div className="flex flex-col items-center gap-3">
+              <p className="text-white text-sm">Or Sign in with</p>
+ 
+              <div className="flex items-center gap-4">
+                <a
+                  href="/auth/google"
+                  className="flex items-center gap-2 px-2 py-1 rounded-md shadow-md transition-transform duration-200 hover:scale-105 hover:shadow-lg"
+                >
+                  <FcGoogle className="w-5 h-5" />
+                </a>
+ 
+                <a
+                  href="/auth/facebook"
+                  className="flex items-center gap-2 px-2 py-1 rounded-md shadow-md transition-transform duration-200 hover:scale-105 hover:shadow-lg"
+                >
+                  <FaFacebook className="w-5 h-5" />
+                </a>
+              </div>
+            </div>
+ 
             <div className="flex justify-center">
+              <p className="text-sm px-2 text-white">Dont have an account?</p>
               <a href="#" className="text-green-400 hover:underline text-sm">
-                Sign Up / Register
+                Register
               </a>
             </div>
           </form>
         </div>
       </div>
     </div>
-    
   );
 }
 
